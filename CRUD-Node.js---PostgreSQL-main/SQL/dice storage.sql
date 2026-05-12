e985d6c8d6169ca56c29e76429009e7a081d91a2
Crowzinho <crowstarstest2@gmail.com>
2026-05-12 16:41:13 -0300

    Fixed the delete on pedidos

--- CRUD-Node.js---PostgreSQL-main/SQL/dice storage.sql
+++ CRUD-Node.js---PostgreSQL-main/SQL/dice storage.sql
@@ -1,13 +1,24 @@
-CREATE TABLE IF NOT EXISTS clientes (
-  id SERIAL PRIMARY KEY,
-  nome VARCHAR(255) NOT NULL,
-  email VARCHAR(255) UNIQUE NOT NULL
-);
 
-CREATE TABLE IF NOT EXISTS pedidos (
-  id SERIAL PRIMARY KEY,
-  produto VARCHAR(255) NOT NULL,
-  valor NUMERIC(10,2) NOT NULL,
-  status VARCHAR(50) DEFAULT 'pendente',
-  cliente_id INTEGER REFERENCES clientes(id) on delete cascade
-);
+-- CREATE TABLE IF NOT EXISTS clientes (
+--   id SERIAL PRIMARY KEY,
+--   nome VARCHAR(255) NOT NULL,
+--   email VARCHAR(255) UNIQUE NOT NULL
+-- );
+
+-- CREATE TABLE IF NOT EXISTS pedidos (
+--   id SERIAL PRIMARY KEY,
+--   produto VARCHAR(255) NOT NULL,
+--   valor NUMERIC(10,2) NOT NULL,
+--   status VARCHAR(50) DEFAULT 'pendente',
+--   cliente_id INTEGER REFERENCES clientes(id) on delete cascade 
+-- );
+
+-- ALTER TABLE pedidos
+-- DROP CONSTRAINT pedidos_cliente_id_fkey;
+
+-- ALTER TABLE pedidos
+-- ADD CONSTRAINT pedidos_cliente_id_fkey
+-- FOREIGN KEY (cliente_id)
+-- REFERENCES clientes(id)
+-- ON DELETE CASCADE;
+
